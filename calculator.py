4bc836a0b3370443d1b9aef1088587b4a9116b68
geoffrey <geoffreygroovy@gmail.com>
2025-03-12 18:58:18 +0300

    few changes

--- calculator.py
+++ calculator.py
@@ -1,22 +1,22 @@
-# Get user input
+# Geting  user input
 num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
 operation = input("Enter an operation (+, -, *, /): ")
 
-# Perform the chosen operation
+#operation
 if operation == '+':
     result = num1 + num2
-    print(f"{num1} + {num2} = {result}")
+    print("{num1} + {num2} = {result}")
 elif operation == '-':
     result = num1 - num2
-    print(f"{num1} - {num2} = {result}")
+    print("{num1} - {num2} = {result}")
 elif operation == '*':
     result = num1 * num2
-    print(f"{num1} * {num2} = {result}")
+    print("{num1} * {num2} = {result}")
 elif operation == '/':
     if num2 != 0:
         result = num1 / num2
-        print(f"{num1} / {num2} = {result}")
+        print("{num1} / {num2} = {result}")
     else:
         print("Error: Division by zero is not allowed.")
 else:
